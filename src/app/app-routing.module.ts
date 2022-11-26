59e1c14b69fdb6aaf80fd655067739922e60a4ac
ali neji <alinj161@gmail.com>
2022-11-26 17:33:06 +0100

    v3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './online/home/home.component';
 import { PostServiceDemComponent } from './online/user/post-service-dem/post-service-dem.component';
-import { EditPostComponent } from './online/user/edit-post/edit-post.component';
+import { EditServiceOffComponent } from './online/user/edit-service-off/edit-service-off.component';
 import { ProfilComponent } from './online/user/profil/profil.component';
 import { Error404Component } from './error404/error404.component';
 import { MessagerieComponent } from './online/messagerie/messagerie.component';
 import { EditProfilComponent } from './online/user/edit-profil/edit-profil.component';
+import { EditServiceDemComponent } from './online/user/edit-service-dem/edit-service-dem.component';
 
 const routes: Routes = [
   {path:'servicesOff',component:HomeComponent},
@@ -16,7 +17,9 @@ const routes: Routes = [
   {path:'servicesDem',component:ServiceDemandeComponent},
   {path:'postservicesOff',component:PostServiceOffComponent},
   {path:'postservicesDem',component:PostServiceDemComponent},
-  {path:'dashborad/edit-service/:id',component:EditPostComponent},
+  {path:'dashborad/edit-service-offert/:id',component:EditServiceOffComponent},
+  {path:'dashborad/edit-service-demande/:id',component:EditServiceDemComponent},
+
   {path:'dashborad/profil',component:ProfilComponent},
   {path:'dashborad/edit-profil/:id',component:EditProfilComponent},
 
--- src/app/online/user/profil/profil.component.ts
+++ src/app/online/user/profil/profil.component.ts
@@ -14,7 +14,8 @@ export class ProfilComponent implements OnInit,OnDestroy {
   constructor( private crud:CrudService ) { }
   getServiceDem!: Subscription;
   getServiceOffert!: Subscription;
-
+  hideOff:boolean=false
+  hideDem:boolean=true
   listServiceDem : Servicesdem[]=[]
   listServiceOffert : Servicesoff[]=[]
 
@@ -23,7 +24,28 @@ export class ProfilComponent implements OnInit,OnDestroy {
   this.crud.deleteService(id)
 }
 
+hidden()
+{
+
+
+if((<HTMLInputElement>document.getElementById("hidden1")).checked==false)
+{
+  this.hideDem=true 
+  this.hideOff=false
+
+}
+else 
+{
+  this.hideDem=false
+  this.hideOff=true
+
+}
+}
+
+
   ngOnInit(): void {
+
+//hidden elements 
     this.getServiceDem=this.crud.getAllServicesDem()
     .subscribe( data => {
       this.listServiceDem = data;
@@ -35,10 +57,6 @@ export class ProfilComponent implements OnInit,OnDestroy {
     });
   }
 
-
-
-
-
   
   ngOnDestroy(): void {
     this.getServiceDem.unsubscribe();
@@ -46,4 +64,8 @@ export class ProfilComponent implements OnInit,OnDestroy {
 
   }
 
+}
+
+function hidden() {
+  throw new Error('Function not implemented.');
 }
